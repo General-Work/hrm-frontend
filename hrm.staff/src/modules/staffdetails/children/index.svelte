a5b2003b8afd4312f602303ea6d267e63946522c
Theo <adusman100@gmail.com>
2025-08-31 20:29:02 +0000

    updated

--- hrm.staff/src/modules/staffdetails/children/index.svelte
+++ hrm.staff/src/modules/staffdetails/children/index.svelte
@@ -11,6 +11,7 @@
 	import bg from '$lib/images/chat1.png';
 	import { onMount } from 'svelte';
 	import PageLoader from '$cmps/ui/pageLoader.svelte';
+	import { Alert } from 'flowbite-svelte';
 
 	let showForm = false;
 
@@ -20,6 +21,7 @@
 
 	let data: any[] = [];
 	let isLoading = true;
+	let readonly = false;
 
 	async function handleClose() {
 		toggleForm();
@@ -33,8 +35,8 @@
 				showError(ret.message);
 				return;
 			}
-			console.log(ret);
 			data = ret.data.children;
+			readonly = ret.data.status === 'PENDING';
 		} catch (error: any) {
 			showError(error?.message || error);
 		}
@@ -52,46 +54,18 @@
 	</div>
 {:else}
 	<div class="flex flex-col gap-4 w-full h-full">
-		<div class:hidden={!Boolean(data.length > 0)} class="flex justify-end">
-			<Button
-				label="Add New Child"
-				color="primary"
-				leadingIcon="line-md:plus-circle"
-				on:click={toggleForm}
-			/>
-		</div>
-		<!-- {#if data.length}
-		<div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
-			{#each data as { childName, dateOfBirth, gender }}
-				<div
-					class="shadow-md rounded-[5px] p-2 flex items-center gap-4 border border-pink-300 bg-pink-50/20"
-				>
-					<div>
-						<div class="bg-white loginbox rounded-full p-2 w-12 h-12 grid place-content-center">
-							<iconify-icon
-								icon="fa6-solid:children"
-								style="font-size: 20px;"
-								class="text-pink-500"
-							/>
-						</div>
-					</div>
-					<div class="">
-						<div class="text-lg font-semibold">
-							{childName}
-						</div>
-						<div>
-							<span>Date of birth:</span>
-							{dayjs(dateOfBirth).format('DD-MMM-YYYY')}
-						</div>
-						<div>
-							<span>Gender:</span>
-							{gender}
-						</div>
-					</div>
-				</div>
-			{/each}
-		</div>
-	{/if} -->
+		{#if readonly}
+			<Alert color="blue">Your children information is under review</Alert>
+		{:else}
+			<div class:hidden={!Boolean(data.length > 0)} class="flex justify-end">
+				<Button
+					label="Add New Child"
+					color="primary"
+					leadingIcon="line-md:plus-circle"
+					on:click={toggleForm}
+				/>
+			</div>
+		{/if}
 
 		{#if data.length > 0}
 			<div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
